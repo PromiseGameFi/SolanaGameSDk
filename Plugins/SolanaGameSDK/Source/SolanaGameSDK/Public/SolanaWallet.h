1d7d366e94c097df1209d42761accd5edc9bc2b5
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2024-08-15 11:26:19 +0100

    refactor(wallet): remove legacy Blueprint methods

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Public/SolanaWallet.h
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Public/SolanaWallet.h
@@ -9,12 +9,6 @@ class SOLANAGAMESDK_API USolanaWallet : public UBlueprintFunctionLibrary
 {
     GENERATED_BODY()
 
-public:
-    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
-    static bool CreateWallet();
-
-    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
-    static FString GetWalletAddress();
 };
 
 //gameName
