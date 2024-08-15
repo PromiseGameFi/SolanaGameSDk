ac2c62d1dddbecc2137ec2a40a343d6d8e39b0e4
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2024-08-15 11:26:54 +0100

    feat(wallet): restore wallet API and metadata

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Public/SolanaWallet.h
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Public/SolanaWallet.h
@@ -1,11 +1,49 @@
+// SolanaWallet.h
 
+#pragma once
 
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Http.h"
+#include "Json.h"
+#include "SolanaWallet.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWalletInfo
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly, Category = "Solana|Wallet")
+    FString PublicKey;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Solana|Wallet")
+    FString PrivateKey;
+};
 
 UCLASS()
 class SOLANAGAMESDK_API USolanaWallet : public UBlueprintFunctionLibrary
 {
     GENERATED_BODY()
 
-};
+public:
+    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
+    static bool CreateWallet(FWalletInfo& OutWalletInfo);
 
-//gameName
+    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
+    static FString GetWalletAddress();
+
+    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
+    static float GetWalletBalance();
+
+    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
+    static bool ImportWallet(const FString& PrivateKey, FWalletInfo& OutWalletInfo);
+
+    UFUNCTION(BlueprintCallable, Category = "Solana|Wallet")
+    static bool ExportWallet(FString& OutPrivateKey);
+
+private:
+    static FWalletInfo CurrentWallet;
+    static FString SolanaRPCUrl;
+
+    static TSharedPtr<FJsonObject> SendRPCRequest(const FString& Method, const TArray<TSharedPtr<FJsonValue>>& Params);
+};
