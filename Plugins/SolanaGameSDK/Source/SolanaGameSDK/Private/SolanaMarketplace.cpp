c5399406c56c58d7dafb32cbc33a4de4ea1451f4
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2024-09-21 03:15:47 +0100

    refactor(marketplace): remove item listing implementation

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
@@ -1,22 +1,6 @@
 #include "SolanaMarketplace.h"
 #include "Http.h"
 
-void USolanaMarketplace::ListItemForSale(FString ItemID, float Price, const FString& Callback)
-{
-    // Implement item listing logic here
-    // This would involve creating a listing instruction, signing it, and sending it to the network
-    // For now, we'll just simulate an HTTP request
-    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-    Request->OnProcessRequestComplete().BindStatic(&USolanaMarketplace::OnResponseReceived, Callback);
-    Request->SetURL("https://api.mainnet-beta.solana.com");
-    Request->SetVerb("POST");
-    Request->SetHeader("Content-Type", "application/json");
-
-    FString JsonString = FString::Printf(TEXT("{\"method\":\"listItem\",\"params\":[\"%s\",%f]}"), *ItemID, Price);
-    Request->SetContentAsString(JsonString);
-
-    Request->ProcessRequest();
-}
 
 void USolanaMarketplace::BuyItem(FString ItemID, const FString& Callback)
 {
