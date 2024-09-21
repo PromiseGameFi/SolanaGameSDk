209a58dd63e60885f4447433029842d0bb0f2ff0
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2024-09-21 03:16:35 +0100

    refactor(marketplace): remove response handling branch

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
@@ -27,15 +27,4 @@ void USolanaMarketplace::BuyItem(FString ItemID, const FString& Callback)
 
 void USolanaMarketplace::OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful, FString Callback)
 {
-    if (bWasSuccessful && Response.IsValid())
-    {
-        // Process the response
-        FString ResponseString = Response->GetContentAsString();
-        // Call the Callback function in Blueprints with the response
-        // You'll need to implement a way to call Blueprint functions from C++
-    }
-    else
-    {
-        // Handle error
-    }
 }
