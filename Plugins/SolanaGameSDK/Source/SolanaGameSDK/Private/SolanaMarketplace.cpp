02cff93de5c4acd9c2f90147d37adf09b9ed4d21
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2024-10-04 13:21:17 +0100

    refactor(marketplace): replace request dispatch with placeholder

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaMarketplace.cpp
@@ -13,9 +13,7 @@ void USolanaMarketplace::ListItemForSale(FString ItemID, float Price, const FStr
     Request->SetHeader("Content-Type", "application/json");
 
     FString JsonString = FString::Printf(TEXT("{\"method\":\"listItem\",\"params\":[\"%s\",%f]}"), *ItemID, Price);
-    Request->SetContentAsString(JsonString);
-
-    Request->ProcessRequest();
+    R
 }
 
 
