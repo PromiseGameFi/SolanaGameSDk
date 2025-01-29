3054fb62724f3fec7f4f006e5bd5e28ea33efe8c
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-01-29 18:03:56 +0100

    refactor(wallet): remove RPC request serialization

--- Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaWallet.cpp
+++ Plugins/SolanaGameSDK/Source/SolanaGameSDK/Private/SolanaWallet.cpp
@@ -84,43 +84,7 @@ bool USolanaWallet::ExportWallet(FString& OutPrivateKey)
 
 TSharedPtr<FJsonObject> USolanaWallet::SendRPCRequest(const FString& Method, const TArray<TSharedPtr<FJsonValue>>& Params)
 {
-    TSharedPtr<FJsonObject> RequestObj = MakeShared<FJsonObject>();
-    RequestObj->SetStringField("jsonrpc", "2.0");
-    RequestObj->SetStringField("id", "1");
-    RequestObj->SetStringField("method", Method);
-    RequestObj->SetArrayField("params", Params);
-
-    FString RequestBody;
-    TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&RequestBody);
-    FJsonSerializer::Serialize(RequestObj.ToSharedRef(), Writer);
-
-    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
-    HttpRequest->SetURL(SolanaRPCUrl);
-    HttpRequest->SetVerb("POST");
-    HttpRequest->SetHeader("Content-Type", "application/json");
-    HttpRequest->SetContentAsString(RequestBody);
-
-    HttpRequest->ProcessRequest();
-
-    // Wait for the request to complete
-    while (HttpRequest->GetStatus() == EHttpRequestStatus::Processing)
-    {
-        FPlatformProcess::Sleep(0.01);
-    }
-
-    if (HttpRequest->GetStatus() == EHttpRequestStatus::Succeeded)
-    {
-        TSharedPtr<FJsonObject> JsonObject;
-        TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpRequest->GetContentAsString());
-        if (FJsonSerializer::Deserialize(Reader, JsonObject))
-        {
-            TSharedPtr<FJsonObject> Result;
-            if (JsonObject->TryGetObjectField("result", Result))
-            {
-                return Result;
-            }
-        }
-    }
+  
 
     return nullptr;
 }
